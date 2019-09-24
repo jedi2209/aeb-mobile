52081971406533bee2d48869362a37826a169595
anyatyu <anyatyu@yandex-team.ru>
2019-09-24 20:02:37 +0300

    first i18n not work

--- src/App.js
+++ src/App.js
@@ -20,6 +20,39 @@ import EventPage from './screens/EventPage';
 
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+import * as RNLocalize from 'react-native-localize';
+import i18n from 'i18n-js';
+import memoize from 'lodash.memoize'; // Use for caching/memoize for better performance
+
+import { I18nManager } from 'react-native';
+
+const translationGetters = {
+  // lazy requires (metro bundler does not support symlinks)
+  en: () => require('./translations/en.json')
+};
+
+const translate = memoize(
+  (key, config) => i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key)
+);
+
+const setI18nConfig = () => {
+  // fallback if no available language fits
+  const fallback = { languageTag: 'en', isRTL: false };
+
+  const { languageTag, isRTL } =
+    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
+    fallback;
+
+  // clear translation cache
+  translate.cache.clear();
+  // update layout direction
+  I18nManager.forceRTL(isRTL);
+  // set i18n-js config
+  i18n.translations = { [languageTag]: translationGetters[languageTag]() };
+  i18n.locale = languageTag;
+};
+
 const MainNavigator = createStackNavigator(
   {
     Home: { screen: NewsScreen },
@@ -55,6 +88,24 @@ const RootStack = createStackNavigator(
 const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    setI18nConfig(); // set initial config
+  }
+
+  componentDidMount() {
+    RNLocalize.addEventListener('change', this.handleLocalizationChange);
+  }
+
+  componentWillUnmount() {
+    RNLocalize.removeEventListener('change', this.handleLocalizationChange);
+  }
+
+  handleLocalizationChange = () => {
+    setI18nConfig();
+    this.forceUpdate();
+  };
+
   render() {
     return <AppContainer />;
   }
