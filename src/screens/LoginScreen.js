8bef265bf7f4a6ed4f7769ef086d2b4cfeae4753
anyatyu <anyatyu@yandex-team.ru>
2020-01-16 14:39:09 +0300

    Login screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  SafeAreaView
+} from 'react-native';
+
+import Logo from '../images/logo-full-white.svg';
+import {TextInput} from '../components/Textinput';
+
+export const LoginScreen = () => {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('../images/login-bg.png')}>
+      <SafeAreaView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.inner}>
+            <Logo style={styles.logo} />
+            <View style={styles.container}>
+              <View style={styles.containerInner}>
+                <View style={styles.field}>
+                  <TextInput label="Email" value="fake@gmail.com" />
+                </View>
+                <View style={styles.field}>
+                  <TextInput label="Password" />
+                </View>
+                <TouchableOpacity
+                  style={[styles.button, {alignItems: 'flex-end'}]}>
+                  <Text style={[styles.buttonText, {color: '#000'}]}>
+                    Forgot your password?
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.separator} />
+              <TouchableOpacity style={styles.button}>
+                <Text style={[styles.buttonText, {textTransform: 'uppercase'}]}>
+                  Sign In
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+        <View style={{marginTop: 80}}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={[styles.buttonText, {color: '#9B9B9B'}]}>
+              Don't have an account?{' '}
+              <Text style={{color: '#000'}}>Sign up</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: 356
+  },
+  inner: {
+    paddingHorizontal: 30,
+    alignItems: 'center'
+  },
+  logo: {
+    marginVertical: 40
+  },
+  container: {
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    width: '100%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 13.16,
+    elevation: 20
+  },
+  containerInner: {
+    padding: 20
+  },
+  separator: {
+    height: 2,
+    width: '100%',
+    backgroundColor: '#024b9e'
+  },
+  field: {
+    marginBottom: 24
+  },
+  button: {
+    height: 56,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#024b9e',
+    fontSize: 15
+  }
+});
