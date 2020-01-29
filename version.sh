75139063cc0bd048ee728361015869a7632cd31f
Aleksandr Kvasha <het2000@mail.ru>
2020-01-29 17:01:58 +0300

    Fix for version script

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -e
 
-PROJECT_DIR="ios/atlantm"
+PROJECT_DIR="ios/Aebrus"
 ONESIGNAL_DIR="ios/OneSignalNotificationServiceExtension"
 INFOPLIST_FILE="Info.plist"
 INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
