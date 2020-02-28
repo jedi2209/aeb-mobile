224efa3eea5c01facf94cf2577d128d11a7c34a6
Aleksandr Kvasha <het2000@mail.ru>
2020-02-28 21:23:12 +0300

    Added APP version header for all requests to API

--- src/core/server.js
+++ src/core/server.js
@@ -1,6 +1,7 @@
 export const API = class AebApi {
   constructor(opts) {
     // :)
+    var pkg = require('../../package.json');
     const apiKeys = {
       android: '6g?-1)!m.8`EEko4OguYsuxpZI)$jM',
       ios: 'z{YT>3}/+vL1a&J_uPM|+f=b)`c&Jx'
@@ -9,7 +10,8 @@ export const API = class AebApi {
 
     this._url = `https://api.aebrus.ru/${lang}`;
     this._headers = {
-      'x-api-key': apiKeys[platform]
+      'x-api-key': apiKeys[platform],
+      'x-app-ver': pkg.version.replace(/\./g, '')
     };
   }
 
